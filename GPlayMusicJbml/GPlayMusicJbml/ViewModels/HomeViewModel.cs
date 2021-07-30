53d25ffc5a2ab5c087f6ef15de09b26da3af537b
Bladimir Miranda Luna <jbmirandal@hotmail.com>
2021-07-30 16:08:38 -0500

    x:DataType="model:SquareCard"

--- GPlayMusicJbml/GPlayMusicJbml/ViewModels/HomeViewModel.cs
+++ GPlayMusicJbml/GPlayMusicJbml/ViewModels/HomeViewModel.cs
@@ -18,14 +18,14 @@ namespace GPlayMusicJbml.ViewModels
                     Title = "Top Albums",
                     SubTitle = "OASIS",
                     Details = "Album by Bad Bunny & J Balvin • 8 songs",
-                    ImageUrl = "https://www.radioactiva.cl/wp-content/uploads/2019/07/bad.jpg"
+                    ImageUrl = "https://yt3.ggpht.com/a/AGF-l79MTIhLC8Y-EFKq2Kl7FaqihciB3geKMPnbNw=s288-c-k-c0xffffffff-no-rj-mo"
                 },
                 new HomeCard
                 {
                     Title = "Recommended new releases",
                     SubTitle = "Portratit of The Dogg",
                     Details = "Album by Snoop Dogg • 12 songs",
-                    ImageUrl = "https://direct.rhapsody.com/imageserver/images/alb.394056178/500x500.jpg"
+                    ImageUrl = "https://yt3.ggpht.com/a/AGF-l79MTIhLC8Y-EFKq2Kl7FaqihciB3geKMPnbNw=s288-c-k-c0xffffffff-no-rj-mo"
                 },
                 new HomeCard
                 {
